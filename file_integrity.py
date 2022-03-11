e6237f34afeb4eb5da727385229053c5ad9a4fa1
awgcooper <adamgold@hotmail.com>
2022-03-11 15:43:36 +0100

    third version

--- file_integrity.py
+++ file_integrity.py
@@ -1,25 +1,24 @@
 #!/usr/bin/python3
 # https://codereview.stackexchange.com/questions/147056/short-script-to-hash-files-in-a-directory
 
-import os.path
-import xxhash
 import datetime
-import pickle
+from pathlib import Path
+import xxhash
+import os.path
+import dill
 
 
-def list_files_recursive(path):
-    files = []
-    # r = root, d = directories, f = files
-    for r, d, f in os.walk(path):
-        for file in f:
-            files.append(os.path.join(r, file))
-    lst = [file for file in files]
-    return lst
+# date formatting
+datefmt = lambda x: x.strftime("%Y-%m-%d")
+today = datefmt(datetime.date.today())
+yesterday = datefmt(datetime.date.today() - datetime.timedelta(days=1))
 
 
-def get_hashes():
-    root_path = ('D:\\misc')
-    file_list = list_files_recursive(root_path)
+# create dictionary with list values { filename: [xxhash, modification time] }
+def dict_today():
+    root_path = ('D:/files')
+    p = Path(root_path).glob('**/*')
+    file_list = [x for x in p if x.is_file()]
     filelist_dict = dict.fromkeys(file_list)
     for key_filename in filelist_dict:
         hash_fn = xxhash.xxh128()
@@ -31,28 +30,80 @@ def get_hashes():
     return filelist_dict
 
 
-datefmt = lambda x: x.strftime("%Y-%m-%d")
-today = datefmt(datetime.date.today())
-yesterday = datefmt(datetime.date.today() - datetime.timedelta(days=1))
-file_parts = [ 'D:\\misc\\hashes_', today, yesterday, '.pkl' ]
-file_today = file_parts[0] + file_parts[1] + file_parts[3]
-file_yesterday = file_parts[0] + file_parts[2] + file_parts[3]
-dict_filelist_today = get_hashes()
-with open(file_today, 'wb') as fp:
-    pickle.dump(dict_filelist_today, fp, protocol=pickle.HIGHEST_PROTOCOL)
+# save today's dictionary to file
+def save_today():
+    dict_filelist_today = dict_today()
+    file_today = 'D:/misc/hashes_' + today + '.pickle'
+    with open(file_today, 'wb') as f:
+        dill.dump(dict_filelist_today, f)
 
-#print(get_hashes())
 
-# load pickle file from yesterday back into dictionary
-with open(file_yesterday, 'rb') as f:
-    dict_filelist_yesterday = pickle.load(f)
+# load file from yesterday back into dictionary
+def dict_yesterday():
+    file_yesterday = 'D:/misc/hashes_' + yesterday + '.pickle'
+    with open(file_yesterday, 'rb') as f:
+        dict_filelist_yesterday = dill.load(f)
+    return dict_filelist_yesterday
 
-#print(dict_filelist_yesterday)
 
-# access hashes
-for k, v in dict_filelist_today.items():
-    print(v[0])
+# compare two dictionaries looking for corrupt files
+# defined as same name, same mtime, different hash
+# results printed to text file
+def corrupt_files():
+    newlist = [ ]
+    for d1key, d1val in dict1.items():
+        d2val = (dict2.get(d1key))
+        if d2val == None:
+            pass
+        elif d1val[0] != d2val[0] and d1val[1] == d2val[1]:
+            newlist.append([str(d1key), ['today', [str(d1val[0]),
+            str(d1val[1])]], ['yesterday', [str(d2val[0]), str(d2val[1])]]])
+    heading = 'CORRUPT FILES:'
+    with open('1.txt', 'w') as f:
+        f.write(heading)
+        f.write('\n')
+    with open('1.txt', 'a') as f:
+        if not newlist:
+            a = '{{ NONE }}'
+            f.write(a)
+            f.write('\n')
+        else:
+            for entry in newlist:
+                f.write(str(entry))
+                f.write('\n')
 
-# access mod_time
-for k, v in dict_filelist_today.items():
-    print(v[1])
+
+# dictionaries for today and yesterday converted to sets
+# sets can be subtracted from one another to find differences
+# today - yesterday = new files; yesterday - today = deleted files
+# results printed to text file
+def files_add_del(d1, d2):
+    d1_keys = set(d1.keys())
+    d2_keys = set(d2.keys())
+    added = d1_keys - d2_keys
+    removed = d2_keys - d1_keys
+    heading = 'NEW FILES:'
+    with open('1.txt', 'a') as f:
+        f.write('\n\n')
+        f.write(heading)
+        f.write('\n')
+    with open('1.txt', 'a') as f:
+        for j in sorted(added):
+            a = '{0}\n'.format(str(j).replace(str(j.anchor), ''))
+            f.write(a)
+    heading = 'DELETED FILES:'
+    with open('1.txt', 'a') as f:
+        f.write('\n\n')
+        f.write(heading)
+        f.write('\n')
+    with open('1.txt', 'a') as f:
+        for j in sorted(removed):
+            a = '{0}\n'.format(str(j).replace(str(j.anchor), ''))
+            f.write(a)
+
+
+save_today()                # save today's file list to pickle file
+dict1 = dict_today()        # load today's list from pickle file to dict
+dict2 = dict_yesterday()    # load yesterday's list from pickle file to dict
+corrupt_files()             # check for corrupt files
+files_add_del(dict1, dict2) # check for new and deleted files
